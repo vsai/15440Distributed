0e32db81d7de143bc52c0724229e37e6f13d3e36
VSai <apple@cmu-837993.wv.cc.cmu.edu>
2013-01-27 15:40:12 -0500

    process manager start

--- Lab1Distributed/src/processMigration/MigratableProcesses.java
+++ Lab1Distributed/src/processMigration/MigratableProcesses.java
@@ -2,16 +2,16 @@ package processMigration;
 
 public class MigratableProcesses implements MigratableProcess {
 
-	@Override
+	//@Override
 	public void run() {
 		// TODO Auto-generated method stub
-
+		System.out.println("Running");
 	}
 
-	@Override
+	//@Override
 	public void suspend() {
 		// TODO Auto-generated method stub
-
+		System.out.println("Suspended");
 	}
 
 	/**
@@ -19,7 +19,11 @@ public class MigratableProcesses implements MigratableProcess {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("YOLO SWAG: ");
+		System.out.println(args.length);
+		for (String arg : args) {
+			System.out.println(arg);
+		}
 	}
 
 }
