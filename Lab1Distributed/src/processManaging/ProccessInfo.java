427feac286b3a4946c1e09c8e296c7aff50d1621
Aditya Joshi <adityacoolj@gmail.com>
2013-02-03 01:18:45 -0500

    Added proccessInfo

---
+++ Lab1Distributed/src/processManaging/ProccessInfo.java
@@ -0,0 +1,20 @@
+package processManaging;
+
+import java.util.concurrent.Future;
+import processMigration.MigratableProcess;
+public class ProccessInfo {
+	
+	private Future<?> f;
+	private MigratableProcess p;
+	
+	public ProccessInfo(Future<?> f, MigratableProcess p){
+		this.f=f;
+		this.p=p;
+	}
+	public Future<?> getFuture(){
+		return f;
+	}
+	public MigratableProcess getProcess(){
+		return p;
+	}
+}
