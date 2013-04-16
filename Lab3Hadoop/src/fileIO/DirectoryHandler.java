1ee4696e97ba273e18664582822df0bf3164f281
vsai <vishalsai@gmail.com>
2013-04-16 18:08:09 -0400

    file compression for reducer

--- Lab3Hadoop/src/fileIO/DirectoryHandler.java
+++ Lab3Hadoop/src/fileIO/DirectoryHandler.java
@@ -18,16 +18,16 @@ public class DirectoryHandler {
 	public DirectoryHandler() {
 		
 	}
-	
-	public void createDirectory (String directoryPath) {
-		
-	}
-
-	public boolean collectSubsetFiles(String destFilename, ArrayList<String> filenames) throws IOException {
+	/*
+	 * Collects for all files of a particular key into one destination file
+	 */
+	public boolean collectSubsetFiles(String destFileName, ArrayList<String> filenames) throws IOException {
+		File destFilename = new File(destFileName);
 		FileWriter filestream = new FileWriter(destFilename, true); 
 		BufferedWriter out = new BufferedWriter(filestream);
 		
 		for (String f : filenames) {
+			System.out.println("Reading file: " + f);
 			FileInputStream fstream = new FileInputStream(f);
 			// Get the object of DataInputStream
 			DataInputStream in = new DataInputStream(fstream);
@@ -35,20 +35,24 @@ public class DirectoryHandler {
 			String strLine;
 			
 			while ((strLine = br.readLine()) != null)   {
-				out.write(strLine);
-				
+				System.out.println(strLine + "\n");
+				out.write(strLine + "\n");
+//				System.out.println("just wrote");
 			}	
 		}
 		//Close the output stream
 		out.close();
-		System.out.println("Wrote to: " + destFilename);
+		System.out.println("Wrote to: " + destFilename.getAbsolutePath());
 		return true;
 	}
 	
+	/*
+	 * Collects all files in the source directory into compressed files in the destination directory
+	 */
 	public boolean collectAllFiles(String destDir, String sourceDir, String extension) {
 		ArrayList<String> files = getAllFiles(sourceDir, extension);
 		HashMap<String, ArrayList<String>> allMap = getKeys(files);
-		
+		boolean success = (new File(destDir)).mkdirs();
 		for (String key : allMap.keySet()){
 			try {
 				collectSubsetFiles(destDir + "/" + key, allMap.get(key));
@@ -60,12 +64,19 @@ public class DirectoryHandler {
 		return true;
 	}
 	
+	/*
+	 * Extracts the key depending on the filename. The filename will end with: /key
+	 */
 	public static String extractKey(String filename){
 		String[] parts = filename.split("/");
 		String key = parts[parts.length - 1];
 		return key;
 	}
 	
+	/*
+	 * Creates a hash map of all the keys, mapped to a list of files that are associated
+	 * with each of those keys
+	 */
 	public static HashMap<String, ArrayList<String>> getKeys(ArrayList<String> allFiles) {
 		String key;
 		HashMap <String, ArrayList<String>> keyFiles = new HashMap<String, ArrayList<String>>();
@@ -83,13 +94,16 @@ public class DirectoryHandler {
 		return keyFiles;
 	}
 	
+	/*
+	 * Gets all files in a directory, with a particular extension
+	 */
 	public static ArrayList<String> getAllFiles (String path, String extension) {
 		ArrayList<String> filenames = new ArrayList<String>();
 		File[] allFiles = new File(path).listFiles();
 		  for(File file: allFiles){
 			  if (file.getName().endsWith(extension)){
-				  System.out.println(file.getAbsolutePath());
 				  filenames.add(file.getAbsolutePath());
+				  System.out.println(file.getAbsolutePath());
 			  } else if (file.isDirectory()) {
 				  filenames.addAll(getAllFiles(file.getAbsolutePath(), extension));
 			  }			 
--- Lab3Hadoop/src/testShizz/ReducerSetupTest.java
+++ Lab3Hadoop/src/testShizz/ReducerSetupTest.java
@@ -1,64 +1,15 @@
 package testShizz;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
 
 import fileIO.DirectoryHandler;
 
 public class ReducerSetupTest {
 
 	
-	public static void main(String args[]) {
-//		ArrayList<String> files = getAllFiles(".", ".txt");
-//		for (String f : files) {
-//			System.out.println(f);
-//		}
-//		 
-//		Set<String> keys = getKeys(files);
-//		for (String k : keys) {
-//			System.out.println(k);
-//		}
-//		
-		
+	public static void main(String args[]) {		
 		DirectoryHandler dh = new DirectoryHandler();
-		dh.collectAllFiles(".", ".", ".txt");
-		
-		
-		
-		
+		String jobname = "adslj";
+		dh.collectAllFiles("./CompressResults/"+ jobname, "./TempResults/" +jobname, ".txt");	
 	}
-
-	
-	public static String extractKey(String filename){
-		String[] parts = filename.split("/");
-		String key = parts[parts.length - 1];
-		return key;
-	}
-	
-	public static Set<String> getKeys(ArrayList<String> allFiles) {
-		String key;
-		Set<String> s = new HashSet<String>();
-		for (String filename : allFiles) {
-			key = extractKey(filename);
-			s.add(key);
-		}
-		return s;
-	}
-	
-	public static ArrayList<String> getAllFiles (String path, String extension) {
-		ArrayList<String> filenames = new ArrayList<String>();
-		File[] allFiles = new File(path).listFiles();
-		  for(File file: allFiles){
-			  if (file.getName().endsWith(extension)){
-				  System.out.println(file.getAbsolutePath());
-				  filenames.add(file.getAbsolutePath());
-			  } else if (file.isDirectory()) {
-				  filenames.addAll(getAllFiles(file.getAbsolutePath(), extension));
-			  }			 
-		  }
-		return filenames;
-	}	
 	
 }
